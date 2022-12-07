3a9005f07653d6c7d01cae32b47910c133b8b949
Niall McLoughlin <G00362680@atu.ie>
2022-12-07 09:56:31 +0000

    added tests for both fields

--- src/main/java/ie/atu/Employee.java
+++ src/main/java/ie/atu/Employee.java
@@ -1,5 +1,14 @@
 package ie.atu;
 
 public class Employee {
+    public String name = "1";
+    public String PPS;
 
-}
+    public void addName(String name) {
+
+        }
+
+
+    }
+
+
--- src/test/java/ie/atu/EmployeeTest.java
+++ src/test/java/ie/atu/EmployeeTest.java
@@ -7,20 +7,44 @@ import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
 
 class EmployeeTest {
-Employee myEmp;
+    Employee myEmp;
 
     @BeforeEach
     void setUp() {
         myEmp = new Employee();
+        myEmp.name = "John doe";
+        myEmp.PPS = "123456";
+
     }
+
+    //  @Test
+    //void employeeNameTest()
+    //{
+    //   assertEquals(12, myEmp.name.length());
+    //}
 
     @Test
-    void employeeNameTest()
-    {
-  //      assertEquals(12, myEmp.name.lenght);
+    void testNameConstructor() {
+        if ((myEmp.name.length() > 5) && (myEmp.name.length() < 22)) {
+        }
+        else {
+            throw new IllegalArgumentException("Name length is invalid");
+        }
+
+    }
+    @Test
+    void testPPsConstructor() {
+        if (myEmp.PPS.length() ==6 ) {
+        }
+        else {
+            throw new IllegalArgumentException("Name length is invalid");
+        }
+
     }
 
-    @AfterEach
-    void tearDown() {
-    }
+
+        @AfterEach
+        void tearDown() {
+        }
+
 }
